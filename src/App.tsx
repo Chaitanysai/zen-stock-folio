08277ae937fdcc5a1673f32be713784b21032980
chaitanyasai <chaitanyasaikandula@gmail.com>
2026-03-09 11:39:37 +0530

    chore: add small test comment

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+// Test change: temporary comment added by automation.
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
